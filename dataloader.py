3ebe0837b2e763ec92825e96b5f8844a7a4f6377
DESKTOP-JU60D4G\lizekun <imu.lizekun@gmail.com>
2022-01-03 23:50:04 +0800

    pycharm格式调整

--- dataloader.py
+++ dataloader.py
@@ -6,6 +6,7 @@ import torch
 import torch.utils.data as data
 from util import cv2_imread
 
+
 def augmentation(lr, hr):
     if np.random.random() < 0.5:
         lr = lr[:, ::-1, :, :]
@@ -19,29 +20,32 @@ def augmentation(lr, hr):
 
     return lr, hr
 
+
 def double_crop(lr, hr, scale=4, size=32):
     B, H, W, C = lr.shape
     w0 = np.random.randint(0, W - size)
     h0 = np.random.randint(0, H - size)
-    lr = lr[:, h0 : h0 + size, w0 : w0 + size,:]
+    lr = lr[:, h0: h0 + size, w0: w0 + size, :]
     hr = hr[:, h0 * scale: (h0 + size) * scale, w0 * scale: (w0 + size) * scale, :]
-    
-    return augmentation(lr,hr)
+
+    return augmentation(lr, hr)
+
 
 def double_load(lr, hr, mode='train', crop_size=32, scale=4, num_frame=7):
-    max_frame=len(lr)
+    max_frame = len(lr)
     if mode == 'train':
         idx_st = np.random.randint(0, max_frame - num_frame + 1)
     elif mode == 'eval':
-        idx_st = 15 - num_frame//2
+        idx_st = 15 - num_frame // 2
     index = list(range(idx_st, idx_st + num_frame))
-    
-    lr_img=np.array([cv2_imread(lr[i]) for i in index])
-    hr_img=np.array([cv2_imread(hr[i]) for i in index])
+
+    lr_img = np.array([cv2_imread(lr[i]) for i in index])
+    hr_img = np.array([cv2_imread(hr[i]) for i in index])
     if mode == 'train':
-        lr_img, hr_img=double_crop(lr_img, hr_img, scale=scale, size=crop_size)
+        lr_img, hr_img = double_crop(lr_img, hr_img, scale=scale, size=crop_size)
 
     return lr_img, hr_img
+
 
 def single_aug(hr):
     if np.random.random() < 0.5:
@@ -53,20 +57,22 @@ def single_aug(hr):
 
     return hr
 
+
 def single_crop(hr, scale=4, size=32):
     B, H, W, C = hr.shape
     w0 = np.random.randint(0, W - size * scale + 1)
     h0 = np.random.randint(0, H - size * scale + 1)
     hr = hr[:, h0: h0 + size * scale, w0: w0 + size * scale, :]
-    
+
     return single_aug(hr)
 
-def single_load(hr, mode='train', crop_size=32,scale=4, num_frame=7):
+
+def single_load(hr, mode='train', crop_size=32, scale=4, num_frame=7):
     max_frame = len(hr)
     if mode == 'train':
         idx_st = np.random.randint(0, max_frame - num_frame + 1)
     elif mode == 'eval':
-        idx_st = 15 - num_frame//2
+        idx_st = 15 - num_frame // 2
     index = list(range(idx_st, idx_st + num_frame))
 
     hr_img = np.array([cv2_imread(hr[i]) for i in index])
@@ -74,6 +80,7 @@ def single_load(hr, mode='train', crop_size=32,scale=4, num_frame=7):
         hr_img = single_crop(hr_img, size=crop_size)
 
     return hr_img
+
 
 class loader(data.Dataset):
     def __init__(self, path, data_kind='single', mode='train', scale=4, crop_size=32, num_frame=7):
@@ -87,9 +94,9 @@ class loader(data.Dataset):
 
         if data_kind == 'double':
             lqname = 'input{}'.format(scale)
-            self.lqfiles=[sorted(glob.glob(join(p, lqname, '*'))) for p in paths]
+            self.lqfiles = [sorted(glob.glob(join(p, lqname, '*'))) for p in paths]
         hqname = 'truth'
-        self.hqfiles=[sorted(glob.glob(join(p, hqname, '*'))) for p in paths]
+        self.hqfiles = [sorted(glob.glob(join(p, hqname, '*'))) for p in paths]
 
         self.data_kind = data_kind
         self.crop_size = crop_size
@@ -102,12 +109,12 @@ class loader(data.Dataset):
 
     def __getitem__(self, index):
         if self.data_kind == 'double':
-            LR, HR = double_load(self.lqfiles[index], self.hqfiles[index], mode=self.mode, crop_size=self.crop_size, 
-                                scale=self.scale, num_frame=self.num_frame)
+            LR, HR = double_load(self.lqfiles[index], self.hqfiles[index], mode=self.mode, crop_size=self.crop_size,
+                                 scale=self.scale, num_frame=self.num_frame)
             LR = torch.from_numpy(LR / 255.).float().permute(3, 0, 1, 2)
         else:
-            HR = single_load(self.hqfiles[index], mode=self.mode, crop_size=self.crop_size, 
-                                scale=self.scale, num_frame=self.num_frame)
+            HR = single_load(self.hqfiles[index], mode=self.mode, crop_size=self.crop_size,
+                             scale=self.scale, num_frame=self.num_frame)
         HR = torch.from_numpy(HR / 255.).float().permute(3, 0, 1, 2)
 
         if self.data_kind == 'double':
--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import time
 import numpy as np
 import math
 import random
-from os.path import join,exists
+from os.path import join, exists
 import glob
 from tqdm import tqdm, trange
 import torch
@@ -16,9 +16,11 @@ import torch.backends.cudnn as cudnn
 import torch.optim
 from torch.cuda.amp import autocast, GradScaler
 from torch import autograd
-from util import automkdir, adjust_learning_rate, evaluation, load_checkpoint, save_checkpoint, makelr_fromhr_cuda, test_video
+from util import automkdir, adjust_learning_rate, evaluation, load_checkpoint, save_checkpoint, makelr_fromhr_cuda, \
+    test_video
 import dataloader
 from models.common import weights_init, cha_loss
+
 
 def setup(rank, world_size):
     if sys.platform == 'win32':
@@ -26,7 +28,7 @@ def setup(rank, world_size):
         # backend and FileStore on Windows platform. Set init_method parameter
         # in init_process_group to a local file.
         # Example init_method="file:///f:/libtmp/some_file"
-        init_method="file:///{your local file path}"
+        init_method = "file:///{your local file path}"
 
         # initialize the process group
         dist.init_process_group(
@@ -41,6 +43,7 @@ def setup(rank, world_size):
 
         # initialize the process group
         dist.init_process_group("nccl", rank=rank, world_size=world_size)
+
 
 def train(rank, config):
     torch.cuda.set_device(rank)
@@ -61,45 +64,53 @@ def train(rank, config):
     criterion = criterion.to(device)
 
     dist.barrier()
-    epoch = load_checkpoint(model, config.path.resume, config.path.checkpoint, weights_init = weights_init, rank = rank)
+    epoch = load_checkpoint(model, config.path.resume, config.path.checkpoint, weights_init=weights_init, rank=rank)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=config.train.init_lr, weight_decay=0)
-    
+
     config.train.epoch = epoch
     iter_per_epoch = config.train.iter_per_epoch
     epoch_decay = config.train.epoch_decay
     step = 0
     scaler = GradScaler()
 
-    train_batch_size = config.train.batch_size // world_size + max(min(config.train.batch_size % world_size - rank, 1), 0)
-    train_dataset = dataloader.loader(config.path.train, data_kind=config.data_kind, mode='train', scale=config.model.scale, 
-                                    crop_size=config.train.in_size, num_frame=config.train.num_frame)
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=train_batch_size, shuffle=True, num_workers=config.train.num_workers, 
-                                            pin_memory=True, drop_last=True)
+    train_batch_size = config.train.batch_size // world_size + max(min(config.train.batch_size % world_size - rank, 1),
+                                                                   0)
+    train_dataset = dataloader.loader(config.path.train, data_kind=config.data_kind, mode='train',
+                                      scale=config.model.scale,
+                                      crop_size=config.train.in_size, num_frame=config.train.num_frame)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=train_batch_size, shuffle=True,
+                                               num_workers=config.train.num_workers,
+                                               pin_memory=True, drop_last=True)
 
-    eval_dataset = dataloader.loader(config.path.eval, data_kind=config.data_kind, mode='eval', scale=config.model.scale, 
-                                    num_frame=config.train.num_frame)
-    eval_loader = torch.utils.data.DataLoader(eval_dataset, batch_size=config.eval.batch_size, shuffle=False, num_workers=config.eval.num_workers, 
-                                    pin_memory=True)
+    eval_dataset = dataloader.loader(config.path.eval, data_kind=config.data_kind, mode='eval',
+                                     scale=config.model.scale,
+                                     num_frame=config.train.num_frame)
+    eval_loader = torch.utils.data.DataLoader(eval_dataset, batch_size=config.eval.batch_size, shuffle=False,
+                                              num_workers=config.eval.num_workers,
+                                              pin_memory=True)
     loss_frame_seq = list(range(config.train.sub_frame, config.train.num_frame - config.train.sub_frame))
     alpha = config.train.alpha
 
-    while(epoch < config.train.num_epochs):
+    while (epoch < config.train.num_epochs):
         if step == 0:
-            adjust_learning_rate(config.train.init_lr, config.train.final_lr, epoch, epoch_decay, step % iter_per_epoch, iter_per_epoch, optimizer, True)
+            adjust_learning_rate(config.train.init_lr, config.train.final_lr, epoch, epoch_decay, step % iter_per_epoch,
+                                 iter_per_epoch, optimizer, True)
             if rank == 0:
                 evaluation(model, eval_loader, config)
             time_start = time.time()
 
         for iteration, (img_hq) in enumerate(train_loader):
-            adjust_learning_rate(config.train.init_lr, config.train.final_lr, epoch, epoch_decay, step % iter_per_epoch, iter_per_epoch, optimizer, False)
+            adjust_learning_rate(config.train.init_lr, config.train.final_lr, epoch, epoch_decay, step % iter_per_epoch,
+                                 iter_per_epoch, optimizer, False)
             optimizer.zero_grad()
 
             img_lq, img_hq = makelr_fromhr_cuda(img_hq, config.model.scale, device, config.data_kind)
 
-            with autocast():     #Automatic Mixed Precision Training
+            with autocast():  # Automatic Mixed Precision Training
                 it_all, pre_it_all = model(img_lq, config.train.sub_frame)
-                loss = criterion(it_all, img_hq[:, :, loss_frame_seq]) + alpha * criterion(pre_it_all, img_hq[:, :, loss_frame_seq])
+                loss = criterion(it_all, img_hq[:, :, loss_frame_seq]) + alpha * criterion(pre_it_all,
+                                                                                           img_hq[:, :, loss_frame_seq])
 
             loss_v = loss.detach()
             if (loss_v > 5 or loss_v < 0 or math.isnan(loss_v)) and epoch > 0:
@@ -115,20 +126,21 @@ def train(rank, config):
 
             # print loss per display_iter
             if (step % config.train.display_iter) == 0 and rank == 0:
-                print(time.strftime("%Y-%m-%d %H:%M:%S",time.localtime()), "Epoch[{}/{}]({}/{}): Loss: {:.8f}".format(epoch, config.train.num_epochs, 
-                        step % iter_per_epoch, iter_per_epoch, loss_v))
+                print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+                      "Epoch[{}/{}]({}/{}): Loss: {:.8f}".format(epoch, config.train.num_epochs,
+                                                                 step % iter_per_epoch, iter_per_epoch, loss_v))
                 sys.stdout.flush()
-            
+
             if step % iter_per_epoch == 0:
                 dist.barrier()
                 if rank == 0:
-                    time_cost = time.time()-time_start
+                    time_cost = time.time() - time_start
                     print(f'spent {time_cost} s')
 
                     epoch += 1
                     config.train.epoch = epoch
                     save_checkpoint(model, epoch, config.path.checkpoint)
-                    
+
                 dist.barrier()
                 if rank == 0:
                     evaluation(model, eval_loader, config)
@@ -140,7 +152,7 @@ def train(rank, config):
 
 def test(rank, config):
     print(config.path.test)
-    
+
     torch.cuda.set_device(rank)
     config.device = device = torch.device(f"cuda:{rank}" if torch.cuda.is_available() else "cpu")
     world_size = len(config.gpus)
@@ -153,10 +165,10 @@ def test(rank, config):
     start_epoch = load_checkpoint(model, config.path.resume, config.path.checkpoint, weights_init, rank)
 
     datapath = sorted(glob.glob(join(config.path.test, '*')))
-    datapath = [d for d in datapath if os.path.isdir(d)][rank :: world_size]
+    datapath = [d for d in datapath if os.path.isdir(d)][rank:: world_size]
     seqname = [os.path.split(d)[-1] for d in datapath]
     savepath = [join(config.path.test, d, config.test.save_name) for d in seqname]
-    
+
     for i, d in enumerate(tqdm(datapath)):
         print(d, savepath[i])
         test_video(model, d, savepath[i], config)
