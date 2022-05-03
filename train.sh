8526d44aae73044525525dbe565b701c143074d1
DESKTOP-JU60D4G\lizekun <imu.lizekun@gmail.com>
2022-05-03 17:39:59 +0800

    meta-govsr_4+2_56 第1版

--- train.sh
+++ train.sh
@@ -4,5 +4,5 @@ do
 	python main.py
 #  python -u train1gpu.py >> work_dir/govsr_4+2_56_1gpu.log 2>&1 &
 #  (python main.py >> work_dir/dis1gpu/govsr_4+2_56_dis1gpu.log 2>&1 &)
-	sleep 10
+	sleep 5
 done
--- train_dis1gpu.sh
+++
@@ -1,7 +0,0 @@
-# The training process may collapse, use this to restart training automatically
-for i in $(seq 1 100)
-do
-#	python main.py
-  python -u main.py >> work_dir/dis1gpu/govsr_4+2_56_dis1gpu.log 2>&1 &
-	sleep 5
-done
